99f525f7145e5546b73544359b9d0b47c0a5ea52
RuslT <ruslf2p@yandex.ru>
2022-05-18 01:33:57 +0300

    add Hello World in App.java

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,2 +1,9 @@
-package hexlet.code;public class App {
+package hexlet.code;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
 }

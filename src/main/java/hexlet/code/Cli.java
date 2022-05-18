a53d1be3e0d0b73829362cfba9390d04c0cf0e17
RuslT <ruslf2p@yandex.ru>
2022-05-19 01:30:10 +0300

    modified Cli.java

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -1,2 +1,15 @@
-package hexlet.code;public class Cli {
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+
+    public static void greeting() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("May I have your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+
 }

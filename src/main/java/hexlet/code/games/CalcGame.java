57f1448ff75f11e7916630f5b289baf505672f16
RuslT <ruslf2p@yandex.ru>
2022-05-27 02:02:29 +0300

    modofied CalcGame.java

--- src/main/java/hexlet/code/games/CalcGame.java
+++ src/main/java/hexlet/code/games/CalcGame.java
@@ -1,2 +1,49 @@
-package hexlet.code.games;public class CalcGame {
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+import java.util.Random;
+
+public class CalcGame {
+
+    public static void game() {
+        String requirement = "What is the result of the expression?";
+        String[] gameData = new String[Engine.getGameDataLength()];
+
+        for (int i = 0; i < gameData.length; i = i + 2) {
+            char operator = randomOperator();
+            int firstNumber = Engine.random();
+            int secondNumber = Engine.random();
+            String question = firstNumber + " " + operator + " " + secondNumber;
+            gameData[i] = question;
+            String answer = Integer.toString(calc(firstNumber, secondNumber, operator));
+            gameData[i + 1] = answer;
+        }
+
+        Engine.game(requirement, gameData);
+    }
+
+    public static int calc(int firstNumber, int secondNumber, char operator) {
+        int result;
+
+        switch (operator) {
+            case '+':
+                result = firstNumber + secondNumber;
+                break;
+            case '-':
+                result = firstNumber - secondNumber;
+                break;
+            case '*':
+                result = firstNumber * secondNumber;
+                break;
+            default:
+                result = 0;
+        }
+        return result;
+    }
+
+    public static char randomOperator() {
+        char[] operators = {'+', '-', '*'};
+        Random random = new Random();
+        return operators[random.nextInt(2)];
+    }
 }
